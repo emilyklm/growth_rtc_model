f31a1d93b285660cec679ea9f01cfd398bedf63a
Emily Klimisch <emilyklimisch@MacBook-Air-von-Emily.local>
2026-01-30 13:54:58 +0000

    Update model equations and parameters

--- model_setup.jl
+++ model_setup.jl
@@ -1,1 +1,141 @@
-#Set up of the reaction network model using Catalyst.jl
+# Defined functions
+# Nutrient import function
+imp(et, vt, s, Kt) = et * vt * s / (Kt + s)
+
+# Nutrient metabolism function
+met(em, vm, si, Km) = em * vm * si / (Km + si)
+
+# Transcription function for mr, mt, mm and rtcr -> wx is different for rtcr!, thetatx different for mr
+tx(wx, e, thetatx) = wx * (e / (thetatx + e))
+
+# Transcription function for mq 
+tq(wx, e, thetatx, q, Kq, hq) = tx(wx, e, thetatx) * (1 / (1 + (q / Kq) ^hq))
+
+# Active RtcR
+function active_rtcr(r, l, c, rt, kr)
+    alpha = rt / kr                                                              # Normalised concentration of rt 
+    fa = (1 + alpha)^6 / (l * (1 + c * alpha)^6 + (1 + alpha)^6)                         # Fraction of open RtcR
+    return fa * r
+end
+
+# Transcription function for rtca, rtcb, thetatx different for mr
+function txab(vmax, kh, kdiss, r, l, c, rt, kr, wx, e, thetatx)         # a is not a parameter anymore
+    voc = (vmax * e) / (kh + e)                                           # Conversion rate of sigma
+    sigmao = (active_rtcr(r, l, c, rt, kr) * voc) / kdiss
+    return sigmao * tx(wx, e, thetatx)
+end
+
+# Gamma function
+gamma(e, gmax, Kgamma) = (gmax * e) / (Kgamma + e)
+
+# Translation rate function for all translations -> Add rtc mRNA to buid a ribosome-RNA complex! kc * rh * mx -> cx from tl(nx, kc, rh, mx, gmax, atp, thetatl) = (1 / nx) * kc * rh * mx * ((gmax * atp) / (thetatl + atp)) 
+tl(cx, e, gmax, Kgamma, nx) = cx * (gamma(e, gmax, Kgamma) / nx)
+
+# Energy consumption during translation
+econ(e, gmax, Kgamma, cri, ct, cm, cq, ca, cb, cr) = gamma(e, gmax, Kgamma) * (cri + ct + cm + cq + ca + cb + cr) 
+
+# Tagging rate 
+vtag(a, rd, ktag, ka) = (a * rd * ktag) / (rd + ka)
+
+# Repair rate
+vrep(b, rt, krep, kb) = (b * rt * krep) / (rt + kb)
+
+# Growth function
+lam(e, gmax, Kgamma, cri, ct, cm, cq, ca, cb, cr, m) = (gamma(e, gmax, Kgamma) * (cri + ct + cm + cq + ca + cb + cr)) / m
+
+
+# Set up of the reaction network model using Catalyst.jl
+using Catalyst 
+
+growth_rtc_model = @reaction_network begin
+    # Import nutrient                                   
+    imp(et, vt, s, Kt),                                                 ∅ => si
+
+    # Metabolization nutrient
+    met(em, vm, si, Km),                                                si => ∅
+    met(em, vm, si, Km) * ns,                                           ∅ => e
+
+    # Transcription
+    tx(wri, e, thetari),                                                ∅ => mri
+    tx(we, e, thetatx),                                                 ∅ => mt 
+    tx(we, e, thetatx),                                                 ∅ => mm
+    tq(wq, e, thetatx, q, Kq, hq),                                      ∅ => mq
+    tx(wr, e, thetatx),                                                 ∅ => mr
+    txab(vmax, kh, kdiss, r, l, c, rt, kr, wba, e, thetatx),            ∅ => (ma, mb)
+
+    # degradation -> or use only one?
+    dm,                                                                (mri, mt, mm, mq, mr, ma, mb) --> ∅ 
+
+    # ribosome binding of mRNA
+    kbi,                                                                rh + mri --> cri
+    kub,                                                                cri --> rh + mri
+
+    kbi,                                                                rh + mt --> ct
+    kub,                                                                ct --> rh + mt
+
+    kbi,                                                                rh + mm --> cm
+    kub,                                                                cm --> rh + mm
+
+    kbi,                                                                rh + mq --> cq
+    kub,                                                                cq --> rh + mq
+
+    kbi,                                                                rh + mr --> cr
+    kub,                                                                cr --> rh + mr
+
+    kbi,                                                                rh + ma --> ca
+    kub,                                                                ca --> rh + ma
+
+    kbi,                                                                rh + mb --> cb
+    kub,                                                                cb --> rh + mb
+
+    # Translation 
+    tl(cri, e, gmax, Kgamma, nri),                                      cri => rh + mri + rh
+    tl(ct, e, gmax, Kgamma, nx),                                        ct => rh + mt + et
+    tl(cm, e, gmax, Kgamma, nx),                                        cm => rh + mm + em
+    tl(cq, e, gmax, Kgamma, nx),                                        cq => rh + mq + q
+    tl(cr, e, gmax, Kgamma, nr),                                        cr => rh + mr + r
+    tl(ca, e, gmax, Kgamma, na),                                        ca => rh + ma + a
+    tl(cb, e, gmax, Kgamma, nb),                                        cb => rh + mb + b
+    
+    # no kin -> rh is created via translation
+
+    # Ribosome binding antibiotics
+    abx * kcm,                                                          cri --> zmri
+    abx * kcm,                                                          ct --> zmt
+    abx * kcm,                                                          cm --> zmm
+    abx * kcm,                                                          cq --> zmq
+    abx * kcm,                                                          cr --> zmr
+    abx * kcm,                                                          ca --> zma
+    abx * kcm,                                                          cb --> zmb
+    
+ 
+    # Damage rate -> scale antibiotic concentration with a factor?
+    abx * kdam,                                                         rh --> rd
+    # Or damage of ribosome complex
+    abx * kdam,                                                         (cri, ct, cm, cq, cr, ca, cb) --> (crid, ctd, cmd, cqd, crd, cad, cbd)
+    # Or damage of ribosme-antibiotic complex -> back to zombie complex
+    abx * kdam,                                                         (zmri, zmt, zmm, zmq, zmr, zma, zmb) --> (zmrid, zmtd, zmmd, zmqd, zmrd, zmad, zmbd)
+    # Or damage of ribosme-antibiotic complex and release of antibiotic
+    abx * kdam,                                                         (zmri, zmt, zmm, zmq, zmr, zma, zmb) --> (crid, ctd, cmd, cqd, crd, cad, cbd)
+
+    # Degradation of damaged RNA
+    kdeg,                                                               rd --> ∅
+    kdeg,                                                               (zmrid, zmtd, zmmd, zmqd, zmrd, zmad, zmbd) --> ∅
+    kdeg,                                                               (crid, ctd, cmd, cqd, crd, cad, cbd) --> ∅
+
+    # Tagging
+    vtag(a, rd, ktag, ka),                                              rd => rt
+    vtag(a, rd, ktag, ka),                                              (zmrid, zmtd, zmmd, zmqd, zmrd, zmad, zmbd) => (zmrit, zmtt, zmmt, zmqt, zmrt, zmat, zmbt)
+    vtag(a, rd, ktag, ka),                                              (crid, ctd, cmd, cqd, crd, cad, cbd) --> (crit, ctt, cmd, cqt, crt, cat, cbt)
+
+    # Repair
+    vrep(b, rt, krep, kb),                                              rt => rh 
+    vrep(b, rt, krep, kb),                                              (zmrit, zmtt, zmmt, zmqt, zmrt, zmat, zmbt) => (zmri, zmt, zmm, zmq, zmr, zma, zmb)
+    vrep(b, rt, krep, kb),                                              (crit, ctt, cmd, cqt, crt, cat, cbt) => (cri, ct, cm, cq, cr, ca, cb )
+
+    # Dilution add damage complexes!
+    lam(e, gmax, Kgamma, cri, ct, cm, cq, ca, cb, cr, m),               (rh, et, em, q, a, b, r, si, mri, mt, mm, mq, ma, mb, mr, e, cri, ct, cm, cq, cs, cb, cr, rd, rt, zmri, zmt, zmm, zmq, zma, zmb, zmr) --> ∅
+    
+    # Energy consumption during translation
+    econ(e, gmax, Kgamma, cri, ct, cm, cq, ca, cb, cr)                  e => ∅
+end
--- params.jl
+++ params.jl
@@ -1,1 +1,59 @@
-# Definition of the parameters and initial conditions for the model
+# Definition of the parameters and initial conditions for the model 
+sf = (1e6 * 1 /(6.02e23 * 1e-15))
+
+# vtag_val = 3254 * sf # uM 
+kbi_value =  1 * sf # [cell /min molecs], Growth
+# Kgamma = 7 * sf # [molecs /cell], Growth
+km_value = 1000 * sf # [molecs /cell], Growth
+kq_value = 152219 * sf # [molecs /cell], Growth
+kt_value = 1000 * sf # [molecs], Growth
+s_value = 1e4 * sf # molecules, Growth 
+thetari_value = 426.87 * sf	# [molecs /cell], Growth 
+thetatx_value = 4.38 * sf # [molecs /cell], Growth 
+
+
+
+params = Dict(
+# :vtag => vtag_val
+:c => 0.01, # Unitless, Rtc
+:dm => 0.1, # min−1, Growth
+:hq => 4, # Unitless, Rtc
+:ka => 20, # µM, Rtc
+:kb => 16, # µM, Rtc
+:kbi => kbi_value, # (min µM)−1, Growth
+:kcm => 0.00599, # (min µM)−1, Growth
+:kdeg => 1, # min−1, Rtc
+# :kdam ?
+:kdiss => 0.006, # min−1, Rtc
+:Kgamma => 255.73, # µM, 
+:kh => 250, # µM, Rtc
+:km => km_value, # µM, Growth
+:kq => kq_value, # µM, Growth
+:kr => 0.125, # µM, Rtc
+:krep => 15.67, # min−1, Rtc
+:kt => kt_value, # µM, Growth
+:ktag => 9780, # min−1, Rtc
+:kub => 1, # min−1, Growth
+:l => 50, # Unitless, Rtc
+:m => 1e8, # aa, Growth
+:na => 338, # aa, Rtc
+:nb => 408, # aa, Rtc
+:nr => 532, # aa, Rtc
+:nri => 7459, # aa/molecs, Growth
+:ns => 0.5, # Unitless, Growth
+:nx => 300, # aa/molecs, Growth 
+:s => s_value, # µM, Growth
+:thetari => thetari_value,  # µM, Growth
+:thetatx => thetatx_value, # µM, Growth
+:vm	=> 5800, # min−1, Growth 
+:vmax => 39.51, # min−1, Rtc
+:vt => 726, # min−1, Growth
+:wba => 5·10−5, # min−1, Rtc
+:we => 0.0069, # µM·min−1, Growth
+:wr => 1·10−6, # µM·min−1, Rtc
+:wri => 1.54, # µM·min−1, Growth
+:wq	=> 1.58, # µM·min−1, Growth
+
+u0 = Dict(
+
+)
